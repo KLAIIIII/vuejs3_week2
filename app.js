7a01cce56dc7050011ed2392e84e69adfb45cd97
KLAIIIII <guyyg03546@gmail.com>
2022-10-27 16:29:11 +0700

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            message: "Hello World!",
+            fname: "Mark Zukerberg",
+            count: 0,
+        }
+    }
+}).mount("#app")
